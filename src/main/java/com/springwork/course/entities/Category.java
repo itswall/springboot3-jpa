b98e3b5e837fba2a2b84e0a2c667a94d95304393
Wall <Wallaceknn@gmail.com>
2024-05-09 18:18:55 -0300

    Product entity

--- src/main/java/com/springwork/course/entities/Category.java
+++ src/main/java/com/springwork/course/entities/Category.java
@@ -3,7 +3,9 @@ package com.springwork.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name ="tb_category")
@@ -16,12 +18,19 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
+    @Transient
+    private Set<Product> products = new HashSet<Product>();
+
     public Category() {
     }
 
     public Category(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     public Long getId() {

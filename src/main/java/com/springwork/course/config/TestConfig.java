040334a7d891e9743957c4cb5a32726bd1051bdf
Wall <Wallaceknn@gmail.com>
2024-05-09 18:56:48 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/springwork/course/config/TestConfig.java
+++ src/main/java/com/springwork/course/config/TestConfig.java
@@ -50,6 +50,15 @@ public class TestConfig implements CommandLineRunner {
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         User u1 = new User(null, "Wallace", "wall@gmail.com", "81970707070", "joji");
         User u2 = new User(null, "Pezao", "pezao@gmail.com", "81960606060", "twice");
 
